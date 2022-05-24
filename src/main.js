92808874a83c2f6a3ec8514087dc949895abcaed
BigBlameGeneral <104887469+BigBlameGeneral@users.noreply.github.com>
2022-05-24 19:53:16 +0800

    modify

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import '@/assets/css/common.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;//阻止启动生产消息
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app')//手动挂载到id为app的dom中
